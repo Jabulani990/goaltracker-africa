a663b85ed5d47a53a05d497bf38e93c5b7f97129
Jabulani990 <72473548+Jabulani990@users.noreply.github.com>
2025-06-07 21:45:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import News from './pages/News';
-import Teams from './pages/Teams';
-import './styles/main.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/news" element={<News />} />
@@ -15,5 +12,3 @@ function App() {
     </BrowserRouter>
   );
 }
-
-export default App;
