ca30ff4ab27b8a9eebf435507833d4e6c607505d
Jabulani990 <72473548+Jabulani990@users.noreply.github.com>
2025-06-07 21:50:27 +0200

    Create Teams.js

---
+++ src/pages/src/pages/Teams.js
@@ -0,0 +1,8 @@
+export default function Teams() {
+  return (
+    <div className="teams-page">
+      <h1>🏆 Teams</h1>
+      <p>Explore your favourite football teams. Coming soon!</p>
+    </div>
+  );
+}
