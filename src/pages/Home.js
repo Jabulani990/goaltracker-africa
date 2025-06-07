175ec352863720c4a54a3a53bdeac7641b4d377e
Jabulani990 <72473548+Jabulani990@users.noreply.github.com>
2025-06-07 21:40:05 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ import LiveScores from '../components/LiveScores';
 
 export default function Home() {
   return (
-    <div className="home">
+    <main className="home">
       <h1>Welcome to GoalTracker Africa</h1>
       <LiveScores />
       <p>More features coming soon!</p>
-    </div>
+    </main>
   );
 }
