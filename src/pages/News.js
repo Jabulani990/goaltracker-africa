ae26cc0f99ddbdae5a840c45c47eac0f33588ef2
Jabulani990 <72473548+Jabulani990@users.noreply.github.com>
2025-06-07 21:49:53 +0200

    Create News.js

---
+++ src/pages/News.js
@@ -0,0 +1,8 @@
+export default function News() {
+  return (
+    <div className="news-page">
+      <h1>📰 Football News</h1>
+      <p>Latest news will appear here. Stay tuned!</p>
+    </div>
+  );
+}
